5374c835d3c27a966dd9993c907685f51069d27d
c2h2 <yiling.cao@gmail.com>
2017-06-17 02:00:42 +0800

    imporve start script.

--- farm.sh
+++ farm.sh
@@ -1,6 +1,8 @@
-cd eth-proxy
-python eth-proxy.py &
-
-sleep 3
+#!/bin/sh
 RIG=`hostname`
+
+cd eth-proxy
+while true; do python eth-proxy.py; sleep 1; done &
+
+sleep 2
 ethminer --farm-recheck 200 -G -F http://127.0.0.1:8080/${RIG}
