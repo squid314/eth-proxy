f0c39fa9bf6c5cb24de58cd357d7f88dd05baddb
Yiling Cao <yiling.cao@gmail.com>
2017-06-23 01:33:54 +0800

    add overclocking to farm scripts.

--- farm.sh
+++ farm.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #ip=`ifconfig | awk '/inet addr/{print substr($2,6)}'|grep 192`
 RIG=`hostname`
-
+PROXY_SRV='127.0.0.1'
+./overclock_gpu.sh
 #while true; do python eth-proxy.py; sleep 1; done &
 
 sleep 2
-ethminer --farm-recheck 200 -G -F http://127.0.0.1:8080/${RIG}
+ethminer --farm-recheck 200 -G -F http://${PROXY_SRV}:8080/${RIG}
