ac4ec1d3169bb39a7e5649377560c975fbc0025c
Yiling Cao <yiling.cao@gmail.com>
2017-06-23 16:14:22 +0800

    edit to just run farm.sh

--- farm.sh
+++ farm.sh
@@ -2,8 +2,8 @@
 #ip=`ifconfig | awk '/inet addr/{print substr($2,6)}'|grep 192`
 RIG=`hostname`
 PROXY_SRV='127.0.0.1'
-./overclock_gpu.sh
-#while true; do python eth-proxy.py; sleep 1; done &
+#./overclock_gpu.sh
+while true; do python eth-proxy.py; sleep 1; done &
 
 sleep 2
 ethminer --farm-recheck 200 -G -F http://${PROXY_SRV}:8080/${RIG}
