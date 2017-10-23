3e4d7d39bdc0fe798fb9e915c782d92412240abd
squid314 <squid314@users.noreply.github.com>
2017-10-23 03:05:07 -0600

    set up as detached initially

--- run-in-tmux.sh
+++ run-in-tmux.sh
@@ -2,9 +2,9 @@
 
 # tmux configuration to run the proxy and miner
 tmux \
-    new-session 'while true ; do python eth-proxy.py ; sleep 1 ; done' \; \
-    split-window -p 55 'sleep 3 ; ethminer --farm-recheck 200 -G -F http://127.0.0.1:8080/`hostname`' \; \
-    split-window -l 10 \; \
+    new-session -d 'while true ; do python eth-proxy.py ; sleep 1 ; done' \; \
+    split-window -p 60 'sleep 3 ; ethminer --farm-recheck 200 -G -F http://127.0.0.1:8080/`hostname`' \; \
+    split-window -l 8 \; \
     set-option remain-on-exit on
 # Start the proxy in the top pane
 # Start the miner in the middle pane
