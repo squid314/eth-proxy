1f243877463a2682d82ac9e176223d946c5e1b5a
squid314 <squid314@users.noreply.github.com>
2017-10-22 23:22:19 -0600

    add a tmux conf to run proxy and miner

---
+++ run-in-tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# tmux configuration to run the proxy and miner
+tmux \
+    new-session 'while true ; do python eth-proxy.py ; sleep 1 ; done' \; \
+    split-window -p 55 'sleep 3 ; ethminer --farm-recheck 200 -G -F http://127.0.0.1:8080/`hostname`' \; \
+    split-window -l 10 \; \
+    set-option remain-on-exit on
+# Start the proxy in the top pane
+# Start the miner in the middle pane
+# Start a shell in the bottom pane, just in case 
+# Easy restart of any pane if it dies
